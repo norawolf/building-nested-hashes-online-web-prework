59ef3a9dea10f4ee2cc6569e3ac82383f041231d
Nora Vogt <noraevogt@gmail.com>
2018-11-25 01:04:38 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,13 @@
 def third_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
       },
     :capulet => {
-      :patriarch => {},
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
